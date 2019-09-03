e336144d532825100d11100f312eba363cc181b1
liujinyuan <liujinyuan@jimimax.com>
2019-09-03 11:13:09 +0800

    '去除相册模块'

--- src/view/index.js
+++ src/view/index.js
@@ -3,15 +3,15 @@
  * @version: 1.0.0
  * @Author: liujinyuan
  * @Date: 2019-08-08 14:46:00
- * @LastEditors: xieruizhi
- * @LastEditTime: 2019-09-02 14:05:46
+ * @LastEditors: liujinyuan
+ * @LastEditTime: 2019-09-03 11:12:58
  */
 
-import Photo from './photo/index';
+// import Photo from './photo/index';
 import Position from './map/index';
 
 const Jimi = {
-    Photo,
+    // Photo,
     Position,
 };
 
