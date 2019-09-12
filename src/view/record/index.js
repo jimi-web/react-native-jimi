18739f9b0b4e6a7e41a408cc41ede96b2a070069
liujinyuan <liujinyuan@jimimax.com>
2019-09-12 14:13:04 +0800

    '添加录音组件'

---
+++ src/view/record/index.js
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: liujinyuan
+ * @Date: 2019-09-12 11:40:33
+ * @LastEditors: liujinyuan
+ * @LastEditTime: 2019-09-12 14:06:22
+ */
+import React, {Component} from 'react';
+import {View,Text,StyleSheet,Image} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class Record extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View>
+                <Text>这里是录音页面</Text>
+            </View>
+        );
+    }
+}
