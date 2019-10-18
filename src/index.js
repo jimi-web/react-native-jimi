6f7c2b756c45d81aab298c0e0787349191bf5719
xieruizhi <xieruizhi@jimi360.cn>
2019-10-18 14:10:12 +0800

    合并代码

--- src/index.js
+++ src/index.js
@@ -3,13 +3,8 @@
  * @version: 
  * @Author: liujinyuan
  * @Date: 2019-08-26 14:28:40
-<<<<<<< HEAD
  * @LastEditors: xieruizhi
- * @LastEditTime: 2019-10-18 14:05:11
-=======
- * @LastEditors: liujinyuan
- * @LastEditTime: 2019-10-18 10:15:00
->>>>>>> ae38247c30c622ec211d1a404499c8b15262fe28
+ * @LastEditTime: 2019-10-18 14:09:39
  */
 import Icon from './assets';
 import Circle from './components';
